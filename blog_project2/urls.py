d7916b748a93204d6441e1fe447afdf2112ded81
Cobee <uniqtop91@gmail.com>
2017-09-19 20:14:40 +0900

    site practice, first commit

---
+++ blog_project2/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from blog_project2.views import HomeView
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view(), name='home'),
+
+    #url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
+]
