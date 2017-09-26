3f824222b4f53ba4a5fc5f3484f0825aa8bc7903
Cobee <unittop91@gamil.com>
2017-09-26 19:19:40 +0900

    django web service practice

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.core.urlresolvers import reverse
 from tagging.fields import TagField
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class Post(models.Model):
 	title = models.CharField('TITLE', max_length=50)
@@ -13,6 +15,7 @@ class Post(models.Model):
 	create_date = models.DateTimeField('Create Date', auto_now_add = True)
 	modify_date = models.DateTimeField('Modify Date', auto_now = True)
 	tag = TagField()
+	owner = models.ForeignKey(User, null=True)
 
 	class Meta:
 		verbose_name = 'post'
@@ -31,3 +34,8 @@ class Post(models.Model):
 
 	def get_next_post(self):
 		return self.get_next_by_modify_date()
+
+	def save(self, *args, **kwargs):
+		if not self.id:
+			self.slug = slugify(self.title, allow_unicode=True)
+		super(Post, self).save(*args, **kwargs)
